441b04218f93736c974db3a7c72bdaf02bc0558c
« Achille <achille@deribreux.be>
2023-08-16 22:40:56 +0200

    add routing & header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PropertyListComponent } from './property/components/components/property-list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './shared/components/header/header.component';
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
     AppComponent,
-    PropertyListComponent
+    PropertyListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
-  ],
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      {path: 'properties', component: PropertyListComponent}
+  ])],
   providers: [],
   bootstrap: [AppComponent]
 })
